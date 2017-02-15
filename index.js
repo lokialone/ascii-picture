4105a2c9f9d7c0f3cbae917a1213763f80359401
lokialone <648875862@qq.com>
2017-02-15 23:32:41 +0800

    feat: finish

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ let ctx = canvas.getContext('2d')
 // let imgUrl = './assets/img.png'
 
 var img = new Image();
-img.src = "./assets/img3.png";
+img.src = "./assets/img.png";
 img.onload =function(){
-    invert(this);
-    // ascii(this);
+    // invert(this);
+    ascii(this);
 };
 function invert(img) {
     ctx.drawImage(img,0,0);
     //获取图片对象以及元素点的数组
-    var img1 = ctx.getImageData(0, 0, 800, 400);
+    var img1 = ctx.getImageData(0, 0, 500, 400);
     var data = img1.data;
     //转换灰度图
     var arr=["M","N","H","Q","$","O","C","?","7",">","!",":","–",";","'"];
     var result=[];
-    for (var i = 0, len = data.length ; i < len; i += 4) {
+    for (var i = 0, len = data.length ; i < len; i += 16) {
         var avg=(data[i]+ data[i+1]+data[i+2])/3;
         data[i] = avg;
         data[i+1] = avg;
         data[i+2] = avg;
         var num=Math.floor(avg/18);
         result.push(arr[num]);
-        if(i%3200==0&&i!=0){
+        if(i%2000==0&&i!=0){
             result.push("<br>")
         }
     }
@@ -71,11 +71,11 @@ function ascii(img) {
   var result = "";
   var lineIndex = 0;
   ctx.drawImage(img, 0, 0)
-  let imgData = ctx.getImageData(0 , 0, 400, 400);
+  let imgData = ctx.getImageData(0 , 0, img.width, img.height);
   let imgDataArray = imgData.data;
-  for (var lineHeight = 0; lineHeight < img.height; lineHeight += 12){
+  for (var lineHeight = 0; lineHeight < img.height; lineHeight += 4){
       var lineASC = "";
-      for (var lineFlag = 0; lineFlag < img.width; lineFlag += 5){
+      for (var lineFlag = 0; lineFlag < img.width; lineFlag += 2){
           lineIndex = (lineHeight * img.width + lineFlag) * 4;
           var r = imgDataArray[lineIndex];
           var g = imgDataArray[lineIndex + 1];
@@ -88,23 +88,6 @@ function ascii(img) {
   let div = document.getElementById('show')
   div.innerHTML = result;
 }
-
-function ascii2() {
-  var arr=["M","N","H","Q","$","O","C","?","7",">","!",":","–",";","."];
-  var result=[];
-  for (var i = 0, len =imgDataArray.length; i < len; i += 8) {
-      var avg=(imgDataArray[i]+imgDataArray[i+1]+imgDataArray[i+2])/3;
-      var num=Math.floor(avg/18);
-      result.push(arr[num]);
-      if(i%1200==0&&i!=0){
-          result.push("<br>")
-      }
-  }
-  let div = document.getElementById('show')
-  div.innerHTML = result.join()
-}
-
-
 
 
       //  console.log(result);
